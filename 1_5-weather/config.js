fe9f2d32b7a2cbd45f09686308a5ae2512436e31
mea2k <69173130+mea2k@users.noreply.github.com>
2023-01-24 00:00:16 +0300

    Add files via upload

---
+++ 1_5-weather/config.js
@@ -0,0 +1,3 @@
+export const weatherAPIToken = process.env.weatherAPIToken ? process.env.weatherAPIToken : ''
+export const weatherUrl = process.env.weatherUrl ? process.env.weatherUrl : 'http://api.weatherstack.com/current'
+
